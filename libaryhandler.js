9470785158cde5904d1a11b05d0689801899673a
Java sauce <mohammedbrandon204@gmail.com>
2022-11-13 09:39:15 -0800

    moved over to seperate file and added a new cursor

---
+++ libaryhandler.js
@@ -0,0 +1,13 @@
+AOS.init();
+let typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    backSpeed: 60,
+    typeSpeed: 60,
+    loop: true
+  });
+
+
+  new kursor({
+    type: 1
+})
+
